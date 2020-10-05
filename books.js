9551acb558401283df2b5d1533e9e86d5cc7352e
DonnyLVu <donnyvu897@gmail.com>
2020-10-05 16:11:16 -0700

    added static books

---
+++ books.js
@@ -0,0 +1,50 @@
+
+export const books = [
+    {
+        id: 'callOfFive',
+        title: 'Call of Five',
+        author: 'Donya Donson',
+        cover: 'Call of 5.png',
+        price: 99,
+        category : 'Action',
+        description: 'Extreme Counting to Five',
+    },
+    {
+        id: 'spiceAndNice',
+        title: 'Spice And Nice',
+        author: 'Donna Lorraine Articulatekins',
+        cover: 'Nice And Spice.png',
+        price: 3,
+        category : 'Romance',
+        description: 'Spicing up life',
+
+
+    },
+    {
+        id: 'unleashTheOath',
+        title: 'Unleashing the Oath',
+        author: 'King Dov Vucak',
+        cover: 'UnleashOath.png',
+        price: 20,
+        category : 'Romance',
+        description: 'Marriage breakups',
+    },
+    {
+        id: 'theJumpingBone',
+        title: 'The Jumping Bone',
+        author: 'Dawn Knee',
+        cover: 'TheJumpBione.png',
+        price: 1,
+        category : 'Action',
+        description: 'Guy is jumping and broke his bone',
+    },
+    {
+        id: 'theGirlInTheHouse',
+        title: 'The Girl in the House',
+        author: 'Don Key',
+        cover: 'DonKey.png',
+        price: 10,
+        category : 'Horror',
+        description: 'There\'s a girl in a house',
+    },
+];
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,32 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+ import { renderBook } from '../utils.js';
+
+// id: 'callOfFive',
+// title: 'Call of Five',
+// author: 'Donya Donson',
+// cover: 'Call of 5.png',
+// price: 99,
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
+test('Renders call of five', (expect) => {
+    // arrange
+    const book1 = {
+        id: 'callOfFive',
+        title: 'Call of Five',
+        author: 'Donya Donson',
+        cover: 'Call of 5.png',
+        price: 99,
+        category : 'Action',
+        description: 'Extreme Counting to Five',
+    };
+    const expected = `<li class="book"><p class="title">Call of Five</p><p class="author">Donya Donson</p><img class="cover" src="../assets/Call of 5.png"><p class="price">$99.00</p><p class="description">Extreme Counting to Five</p><p class="category">Action</p><button>Add to cart</button></li>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = renderBook(book1);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected);
 });
