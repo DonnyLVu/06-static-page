1822c96d9268071a7e485fcf1c1da106ad2b7834
DonnyLVu <donnyvu897@gmail.com>
2020-10-06 21:36:41 -0700

    added calcOrderItem but not yet added it to total

---
+++ cart/cart-utils.js
@@ -0,0 +1,22 @@
+import { calcLineItem } from './render-line-items.js';
+import { books } from '../books.js';
+
+export function calcOrderTotal(cartArray) {
+
+    let accumulator = 0;
+
+    // for every item in the cart
+    for (let i = 0; i < cartArray.length; i++) {
+        const item = cartArray[i];
+        // go get the item's true data
+        const trueItem = calcLineItem(books, item.id);
+
+        // use the true data's price with the cart's quantity to get the subtotal for this item
+        const subtotal = trueItem.price * item.quantity;
+
+        // add that subtotal to the accumulator
+        accumulator = accumulator + subtotal;
+    }
+
+    return accumulator;
+}
